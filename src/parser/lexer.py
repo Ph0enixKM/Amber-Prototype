106ddf6babd8ca141f700015f9fe8c0fde234ce1
Ph0enixKM <pkaras.it@gmail.com>
2022-01-26 22:59:10 +0100

    fix: rules error

--- src/parser/lexer.py
+++ src/parser/lexer.py
@@ -2,36 +2,62 @@ from tok import Token
 from file_iterator import FileIterator
 from rules import Rules
 
+symbols = ['\n', '\'', '{', '}', '(', ')', '*', '+', '=', '-', '%']
+
 def lexer(code):
     pos = FileIterator()
     rules = Rules(code, pos)
     lexem = []
     word = ''
 
-    def add_word():
+    def add_word(same_line = False):
         if len(word):
-            lexem.append(Token(*pos.get(len(word)), word))
+            lexem.append(Token(*pos.get(len(word) - same_line), word))
             return ''
         return word
 
     for letter in code:
-        if letter.isspace():
-            word = add_word()
-        elif rules.handle_rule():
-            print('Rule handled')
-        else:
+        new_rule = rules.handle_rule()
+
+        if not new_rule and rules.is_region():
             word += letter
+        else:
+            if letter in [' ', '\t']:
+                word = add_word()
+            elif letter in symbols:
+                word = add_word()
+                word += letter
+                word = add_word(True)
+            else:
+                word += letter
+
         pos.next(letter)
     add_word()
     return lexem
 
 if __name__ == '__main__':
+    def test_positioner(code, lexem):
+        lines = code.split('\n')
+        for token in lexem:
+            if token.word == '\n':
+                continue
+            word = token.word
+            row = token.row
+            col = token.col
+            lookup = lines[row - 1][col - 1:col - 1 + len(word)]
+            if word != lookup:
+                print(f'TEST Failed: ({word}) vs ({lookup}) [{row}:{col}]')
+                return False
+        return True
+
     code = [
-        'let a = 12',
+        'let test a  this   = 12',
         'if a == 12 {',
-        '    $ echo "hello world"',
+        '    $ echo \'hello world\'',
         '}'
     ]
     lexem = lexer('\n'.join(code))
-    for tok in lexem:
-        print(tok)
+    test_positioner('\n'.join(code), lexem)
+
+    # for tok in lexem:
+    #     print(tok)
--- src/parser/rules.py
+++ src/parser/rules.py
@@ -8,6 +8,9 @@ class Rule:
     
     def is_region(self):
         return self.region
+    
+    def __str__(self):
+        return f'Rule[{self.name} -> {self.start} {self.end}]'
 
 class RuleCodeChunk:
     def __init__(self, code, file_iter):
@@ -15,20 +18,19 @@ class RuleCodeChunk:
         self.file_iter = file_iter
     
     def get(self):
-        print(self.file_iter.__dict__)
         index = self.file_iter.get_index()
         return self.code[:index + 1]
 
 class Rules:
     def __init__(self, code, file_iter):
         self.chunk = RuleCodeChunk(code, file_iter)
-        self.stack = [Rule('global', None)]
         self.rules = [
+            Rule('global', None, region=False),
             Rule('string_single', '\'', interp='string_interp'),
             Rule('string_interp', '{', '}', region=False),
             Rule('comment', '#', '\n'),
-            Rule('global', None, region=False),
         ]
+        self.stack = [self.rules[0]]
         self.begin_rules = list(map(lambda r: r.start, self.rules))
         self.end_rules = list(map(lambda r: r.end, self.rules))
     
@@ -42,9 +44,11 @@ class Rules:
         return self.get_rule_by_func(lambda r: r.name == name)
 
     def get_rule_by_tok_str(self, chunk, tok_str):
+        is_valid = lambda r: tok_str(r) != None
         is_equal = lambda r: tok_str(r) == chunk[-len(tok_str(r)):]
         is_escaped = lambda r: chunk[-len(tok_str(r)) - 1] == '\\'
-        return self.get_rule_by_func(lambda r: is_equal(r) and not is_escaped(r))
+        return self.get_rule_by_func(
+            lambda r: is_valid(r) and is_equal(r) and not is_escaped(r))
 
     def get_rule_by_start(self, chunk):
         return self.get_rule_by_tok_str(chunk, lambda r: r.start)
@@ -52,14 +56,21 @@ class Rules:
     def get_rule_by_end(self, chunk):
         return self.get_rule_by_tok_str(chunk, lambda r: r.end)
 
+    def is_region(self):
+        return self.stack[-1].region
     
     def handle_rule(self):
         # If it's a region - close it
         if self.stack[-1].is_region():
-            print(f'chunk (5) ...{self.chunk.get()[-5:]}')
             rule = self.get_rule_by_end(self.chunk.get())
+            if rule and rule.name == self.stack[-1].name:
+                self.stack.pop()
+                return True
+        else:
+            # Check if it's an interpolation and can be closed
+            rule = self.get_rule_by_start(self.chunk.get())
             if rule:
-                print(f'New rule: {rule}')
-                # self.stack.append(rule)
-                # TODO: Finish it
+                self.stack.append(rule)
+                return True
+        return False
 
